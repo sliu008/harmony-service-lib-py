75be3fcdcb0775b095521d87c2be0a5e361b9534
Simon Liu <simon.liu@jpl.nasa.gov>
2024-08-19 09:38:23 -0700

    push changes with print

--- harmony/http.py
+++ harmony/http.py
@@ -427,6 +427,8 @@ def download(config, url: str, access_token: str, data, destination_file,
             for chunk in response.iter_content(chunk_size=buffer_size):
                 destination_file.write(chunk)
                 destination_file.flush()
+                print("FLUSHING THE FILE")
+            print("FINISH THE FOR LOOP")
             file_size = os.path.getsize(destination_file.name)
         time_diff = datetime.datetime.now() - start_time
         duration_ms = int(round(time_diff.total_seconds() * 1000))
